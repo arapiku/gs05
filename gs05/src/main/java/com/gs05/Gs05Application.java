cb6a554710ad14db94de888b524821a5373570d6
arapiku <arpaiku420@gmail.com>
2018-01-07 14:12:06 +0900

    first commit

---
+++ gs05/src/main/java/com/gs05/Gs05Application.java
@@ -0,0 +1,12 @@
+package com.gs05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gs05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gs05Application.class, args);
+	}
+}
